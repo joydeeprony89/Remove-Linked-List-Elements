4c2bb8ed9c2b5529193449079421c092508649a2
jd_786 <jaydeep_shil@certree.com>
2022-09-06 10:25:58 +0530

    Add project files.

---
+++ Remove Linked List Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Remove_Linked_List_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null) return null;
+            var dummy = new ListNode();
+            dummy.next = head;
+            var cur = dummy;
+            while (cur.next != null)
+            {
+                if (cur.next.val == val)
+                {
+                    cur.next = cur.next.next;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+            return dummy.next;
+        }
+    }
+}
